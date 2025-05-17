d0b00ce6d040ae5eb4d966eea0861aa7704acd6f
ziadtarek12 <zi3dtarek@gmail.com>
2025-05-17 04:50:29 +0300

    fixing trainer.py

--- opennmt/onmt/trainer.py
+++ opennmt/onmt/trainer.py
@@ -223,7 +223,10 @@ class Trainer(object):
                 train_iter, self.gpu_rank, None, self.n_gpu)
 
         # Initialize step - use optimizer's step counter or 0 if not available
-        step = 0
+        try:
+            step = self.optim.training_step
+        except (AttributeError, TypeError):
+            step = 0
             
         for i, (batches, normalization) in enumerate(
                 self._accum_batches(train_iter)):
