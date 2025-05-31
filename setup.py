0ee8ee4712e7fbb043dd32cca59afe6f7df73ca9
ziadtarek12 <zi3dtarek@gmail.com>
2025-05-31 14:06:03 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -64,10 +64,10 @@ def install_dependencies():
         "matplotlib",
         "pyyaml",
         "numpy",
-        "torchtext=0.16.0",
-        "torch=2.1.0",
-        "torchvision=0.16.0",
-        "torchaudio=2.1.0",
+        "torchtext==0.16.0",
+        "torch==2.1.0",
+        "torchvision==0.16.0",
+        "torchaudio==2.1.0",
         "ipdb",
         "tensorboardX"
     ]
